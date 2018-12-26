4139ae572efd32427e119fb52bf6e7a42065d075
Oleg Kozlov <xxblx@posteo.org>
2018-12-26 14:08:36 +0100

    Fixed a bug in `encsend.utils.get_config_dir`, added docstrings
    Fixed bug in `encsend.utils.get_config_dir`, used to get wrong path in case $XDG_CONFIG_HOME is set. Added docstring to all function in `encsend.utils`.

--- encsend/utils.py
+++ encsend/utils.py
@@ -7,9 +7,19 @@ from nacl.signing import SigningKey
 
 
 def get_config_dir(make_dir=False):
+    """ Get path to encsend's configuration directory.
+    If `$XDG_CONFIG_HOME` is set, use `${XDG_CONFIG_HOME}/encsend`,
+    otherwise - `~/.config/encsend`.
+
+    :param make_dir: make encsend's configuration directory
+        if it doesn't exist
+    :type make_dir: bool
+    :return: path to encsend's configuration directory
+    :rtype: str
+    """
     _evar = 'XDG_CONFIG_HOME'
     if _evar in os.environ and os.environ[_evar]:
-        path = os.environ[_evar]
+        path = os.path.join(os.environ[_evar], 'encsend')
     else:
         path = os.path.join(os.path.expanduser('~'), '.config', 'encsend')
 
@@ -20,13 +30,27 @@ def get_config_dir(make_dir=False):
 
 
 def get_signing_key_path(make_dir=False):
-    """ Get default path to signing key """
+    """ Get default path to signing key
 
+    :param make_dir: make encsend's configuration directory
+        if it doesn't exist
+    :type make_dir: bool
+    :return: default path to signing key file
+    :rtype: str
+    """
     conf_dir_path = get_config_dir(make_dir)
     return os.path.join(conf_dir_path, 'host_signature')
 
 
 def get_signing_key(path=None):
+    """ Get signing key
+
+    :param path: path to signing key file, if path is `None`
+        default path is used
+    :type path: str or None
+    :return: signing key
+    :rtype: nacl.signing.SigningKey
+    """
     if path is None:
         path = get_signing_key_path()
     with open(path, 'rb') as f:
@@ -50,6 +74,12 @@ def get_verify_key_hex(path=None):
 
 
 def create_signing_key(path=None):
+    """ Generate and write to file new signing key
+
+    :param path: path to signature key file, if path is `None`
+        default path is used
+    :type path: str or None
+    """
     if path is None:
         path = get_signing_key_path(make_dir=True)
     signing_key = SigningKey.generate()
